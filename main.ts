d3d619f5237fe44f0f51131e2efcf58476e857cf
Michael Lebedew <michael.lebedew@web-dienstleister.de>
2023-05-24 13:46:23 +0200

    more cors

--- main.ts
+++ main.ts
@@ -6,10 +6,19 @@
 
 import "$std/dotenv/load.ts";
 
-import { start } from "$fresh/server.ts";
+import { start, Middleware } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
+
+
+const corsMiddleware: Middleware = async (ctx, next) => {
+    ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+    ctx.response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    ctx.response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+    await next();
+};
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
